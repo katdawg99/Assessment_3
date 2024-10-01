a170cdaa6994f8034704e22d194597e09c1aad6d
Kat_Trina <167485602+katdawg99@users.noreply.github.com>
2024-10-02 09:58:45 +1000

    9:58

---
+++ Assets/play_pause.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class play_pause : MonoBehaviour
+{
+    // Reference to the AudioSource component
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        // Get the AudioSource component attached to this GameObject
+        audioSource = GetComponent<AudioSource>();
+        
+        // Optionally start playing the audio on start
+        // audioSource.Play();
+    }
+
+    void Update()
+    {
+        // Check for user input to toggle play/pause
+        if (Input.GetKeyDown(KeyCode.Space)) // Change this key as needed
+        {
+            ToggleAudio();
+        }
+    }
+
+    private void ToggleAudio()
+    {
+        if (audioSource.isPlaying)
+        {
+            PauseAudio();
+        }
+        else
+        {
+            PlayAudio();
+        }
+    }
+
+    public void PlayAudio()
+    {
+        audioSource.Play();
+        Debug.Log("Audio Playing");
+    }
+
+    public void PauseAudio()
+    {
+        audioSource.Pause();
+        Debug.Log("Audio Paused");
+    }
+}
